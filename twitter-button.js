3e5afcde6ba8fabd518eb743e52cce6d5a61af13
Nicholas Tinsley <00sturm@gmail.com>
2017-07-29 15:56:44 -0400

    simplify twitter button component

--- twitter-button.js
+++ twitter-button.js
@@ -1,6 +1,4 @@
-const TweetButton = ({ msg }) => {
-	return (<span>
-		<a href="https://twitter.com/share" class="twitter-share-button" data-size="large" data-text={msg} data-url="https://nationalfood.today" data-hashtags="nationalfoodtoday" data-show-count="false">Tweet</a>
-	</span>);
-};
+const TweetButton = ({ msg }) => (<span>
+	<a href="https://twitter.com/share" class="twitter-share-button" data-size="large" data-text={msg} data-url="https://nationalfood.today" data-hashtags="nationalfoodtoday" data-show-count="false">Tweet</a>
+</span>);
 export default TweetButton;
